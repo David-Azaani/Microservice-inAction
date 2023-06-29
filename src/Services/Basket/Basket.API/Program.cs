f245eb3d076ae8e8f3ccca6930f3d40d8b16b69c
f00000b <f00000b@gmail.com>
2023-06-30 01:34:07 +0330

    Basket Repository,Controller,appsetingand ProgramCSProgram

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,11 +1,26 @@
+using Basket.API.Repositories;
+using Basket.API.Repositories.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
+ConfigurationManager configuration = builder.Configuration; // allows both to access and to set up the config
+//IWebHostEnvironment environment = builder.Environment;
 // Add services to the container.
+builder.Services.AddStackExchangeRedisCache(option =>
+{
+    option.Configuration = configuration.GetValue<string>("CacheSettings:ConnectionString");
 
+});
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddSwaggerGen();
+
+
+
 
 var app = builder.Build();
 
