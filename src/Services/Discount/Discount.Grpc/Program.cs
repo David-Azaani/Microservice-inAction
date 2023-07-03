9f54136f43c117367d2771a83a3c6cf31ea48ace
f00000b <f00000b@gmail.com>
2023-07-03 20:13:51 +0330

    Grpc template and preparing it

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,3 +1,6 @@
+using Discount.Grpc.Extentions;
+using Discount.Grpc.Repositories;
+using Discount.Grpc.Repositories.Interfaces;
 using Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,9 +10,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
-
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 var app = builder.Build();
-
+app.MigrateDatabase<Program>();
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
