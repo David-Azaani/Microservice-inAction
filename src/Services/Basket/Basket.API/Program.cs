116f09e8576f5b45982960336ab9450946ddbc0a
f00000b <f00000b@gmail.com>
2023-07-10 19:24:51 +0330

    👀 Adding Masstransit

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -2,6 +2,7 @@ using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Basket.API.Repositories.Interfaces;
 using Discount.Grpc.Protos;
+using MassTransit;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,6 +25,25 @@ builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(
     );
 
 builder.Services.AddScoped<DiscountGrpcService>();
+
+// MassTransit - RabbitMQ Configuration
+builder.Services.AddMassTransit(config =>
+{
+    config.UsingRabbitMq((ctx, cfg) =>
+    {
+        // configuration["EventBusSettings:HostAddress"]
+        cfg.Host( configuration["EventBusSettings:HostAddress"]);
+    });
+
+});
+//builder.Services.AddMassTransitHostedService();
+
+
+
+
+
+
+
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
